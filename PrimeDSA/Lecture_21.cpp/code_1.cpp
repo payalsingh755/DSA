a038bdfd2f5307ebc12b20f98fef028b0bf2e2ad
payalsingh755 <payalbodana13991@gmail.com>
2026-02-12 21:29:13 +0530

    code

--- PrimeDSA/Lecture_21.cpp/code_1.cpp
+++ PrimeDSA/Lecture_21.cpp/code_1.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 int main(){
     int n=5;
-    for(int i=n;i>=1;i--){
-        for(int j=1;j<=i;j++){
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<i;j++){
+            cout<<"  ";
+        }
+        for(int star=0;star<n-i;star++) {
             cout<<"* ";
         }
         cout<<endl;
