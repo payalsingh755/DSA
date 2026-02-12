f624c21edc1654b6a3cb4a6629c22ea52952c915
payalsingh755 <payalbodana13991@gmail.com>
2026-02-12 13:26:49 +0530

    1st Program

---
+++ sample.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter the 1st value: ";
+    cin>>n;
+    int m;
+    cout<<"Enter the 2nd value: ";
+    cin>>m;
+    int k=m+n;
+    cout<<k;
+    cin>>k;
+    cout<<endl;
+    return 0;
+}
