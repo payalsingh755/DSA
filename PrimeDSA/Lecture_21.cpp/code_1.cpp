d10363d05c854a6b7bbe5ac788fffe041ab6ae42
payalsingh755 <payalbodana13991@gmail.com>
2026-02-17 14:37:09 +0530

    number a reverse

--- PrimeDSA/Lecture_21.cpp/code_1.cpp
+++ PrimeDSA/Lecture_21.cpp/code_1.cpp
@@ -2,13 +2,40 @@
 using namespace std;
 int main(){
     int n=5;
-    for(int i=0;i<=n;i++){
-        for(int j=0;j<i;j++){
-            cout<<"  ";
-        }
-        for(int star=0;star<n-i;star++) {
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<n-1;j++)
+            cout<<" ";
+        
+        for(int k=1;k<=i;k++) 
             cout<<"* ";
-        }
+        
+        cout<<endl;
+    }
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<n-1;j++)
+            cout<<" ";
+        
+        for(int k=1;k<=i;k++) 
+            cout<<"* ";
+        
+        cout<<endl;
+    }
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<n-1;j++)
+            cout<<" ";
+        
+        for(int k=1;k<=i;k++) 
+            cout<<"* ";
+        
+        cout<<endl;
+    }
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<n-1;j++)
+            cout<<" ";
+        
+        for(int k=1;k<=i;k++) 
+            cout<<"* ";
+        
         cout<<endl;
     }
     return 0;
