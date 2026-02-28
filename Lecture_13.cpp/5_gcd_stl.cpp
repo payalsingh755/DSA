5996b44728f5ca14edc04070fb8a90a81abab916
payalsingh755 <singhpayal13991@gmail.com>
2026-02-28 23:08:51 +0530

    5 gcd

---
+++ Lecture_13.cpp/5_gcd_stl.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+//__gcd()
+#include<algorithm>
+//-infinity and +infinity
+#include<climits>
+using namespace std;
+
+int main(){
+    int c=INT_MAX;//+INFINTY
+    int d=INT_MIN; //-infinty
+    int a=5;
+    int b=10;
+    cout<<__gcd(a,b);
+    return 0;
+
+}
