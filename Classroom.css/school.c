27b3b603e1fac0832e77d24ed727a9ccdee8fcca
payalsingh755 <singhpayal13991@gmail.com>
2026-06-06 13:28:35 +0530

    couching

---
+++ Classroom.css/school.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main(){
+    int num=0;
+    int age=0;
+    int num1=0;
+
+    if{ printf("Enter your number : ");
+        scanf("%d",&num);
+        return 0;
+       }
+         if{
+        printf("Enter your age : ");
+        scanf("%d",&age);
+        return 0;
+    }
+             if(60>num){
+              printf("Available for admission : ");
+                 if(age<18){
+        printf("But not Available for picnik : ");
+    }
+    }
+                     if(age>18){
+        printf("Available for picnik : ");
+                     else(60<num){
+        printf("Available for admission : ");
+    }
+                         if(60>num && age>18){
+        printf("congratulation for admission and picnik : ")
+                            if()
+    }
+                            else{
+        printf("Go to Home");
+    }
+    return 0;
+}
